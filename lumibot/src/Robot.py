2d56837de44b56070ce97d4fe5ab0577cb9a54f4
jhcsoares <jhcsoares1@gmail.com>
2023-11-08 13:04:24 -0300

    added move2goal method (pid) in Robot.py

--- lumibot/src/Robot.py
+++ lumibot/src/Robot.py
@@ -10,7 +10,15 @@ class Robot():
     def __init__(self):
         rospy.init_node('path', anonymous=True)
         self.velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.pose_subscriber = rospy.Subscriber('pose', Pose, self.callback)
         self.rate = rospy.Rate(10)
+        self.pose = Pose()
+
+    #Callback function implementing the pose value received
+    def callback(self, data):
+        self.pose = data
+        self.pose.x = round(self.pose.x, 4)
+        self.pose.y = round(self.pose.y, 4)
 
     def rotate(self, speed, angle, clockwise):           
         vel_msg = Twist()
@@ -85,12 +93,54 @@ class Robot():
         for m, r in move:
             self.move(m[0], m[1], m[2])
             self.rotate(r[0], r[1], r[2])
+    
+    def move2goal(self):
+        goal_pose = Pose()
+        goal_pose.x = float(input("Set your x goal:"))
+        goal_pose.y = float(input("Set your y goal:"))
+        distance_tolerance = float(input("Set your tolerance:"))
+
+        vel_msg = Twist()
+        
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        
+        dist = self.get_distance(goal_pose.x, goal_pose.y)
+        
+        while dist >= distance_tolerance:
+
+            #Proportional Controller
+            #linear velocity in the x-axis:
+            vel_msg.linear.x = 1.5 * dist
+
+            #angular velocity in the z-axis:
+            ang_to_goal = self.get_angle_to_goal(goal_pose.x, goal_pose.y)
+            ang_dif = (ang_to_goal - self.pose.theta)            
+            if( (ang_dif) >= (pi) ):
+                 ang_dif = (2*pi)-ang_dif
+            if( (ang_dif) < -(pi) ):
+                 ang_dif = ang_dif+(2*pi)             
+            vel_msg.angular.z = 4 * (ang_dif)
+
+            #Publishing our vel_msg
+            self.velocity_publisher.publish(vel_msg)
+            self.rate.sleep()
+            
+            dist = self.get_distance(goal_pose.x, goal_pose.y)
+            
+        #Stopping our robot after the movement is over
+        vel_msg.linear.x = 0
+        vel_msg.angular.z =0
+        self.velocity_publisher.publish(vel_msg)
         
 if __name__ == '__main__':
     try:
         #Testing our function
         x = Robot()
-        x.path()
+        x.move2goal()
 
     except rospy.ROSInterruptException: 
         pass
